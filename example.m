98d89518df0d9bfd6279dc7f7329a53dc2942cf0
gorchard <gorchard@jhu.edu>
2016-01-06 17:29:00 +0800

    bug fixes with shared variable names interfering with the Caltech Toolbox

--- example.m
+++ example.m
@@ -1,5 +1,4 @@
 % Create the calibration images, more the better
-cd Calibrate
 % Use the 'PresentStimulus' function to bring up a calibration pattern on
 % the screen. 
 rectangleSize_mm = PresentStimulus([10,10], 10);
@@ -13,29 +12,41 @@ rectangleSize_mm = PresentStimulus([10,10], 10);
 % This script requires some ATIS functions to be on 
 % the Matlab Path. These functions can be obtained from
 % http://www.garrickorchard.com/code/matlab-AER-functions 
-[TD, APS] = ReadAER(filename);
-%if using EM events:
-image = MakeImage(EM, [304,240], 1);
-%if using TD events:
-image = MakeImage(TD, [304,240], 0);
+ 
+filename = '0002.val'; %an example of a filename
+[TD, EM] = ReadAER(filename);
+%%if using EM events:
+% image = MakeImage(EM, [304,240], 1);
+%%if using TD events:
+calibration_image = MakeImage(TD, [304,240], 0);
 
+imshow(calibration_image); % optionally show the image
 
 %% For DVS
-% This script requires some DVS functions to be on 
-% the Matlab Path. These functions can be obtained from
-% https://svn.code.sf.net/p/jaer/code/scripts/matlab
-[allAddr,allTs]=loadaerdat(filename);
-[TD.x,TD.y,TD.p]=extractRetinaEventsFromAddr(addr);
-TD.x = TD.x+1; % difference in convention between DVS and ATIS
-TD.y = TD.y+1; % difference in convention between DVS and ATIS
-TD.p(TD.p==-1) = 0;
-
-image = MakeImage(TD, [128,128], 0);
+% % This script requires some DVS functions to be on 
+% % the Matlab Path. These functions can be obtained from
+% % https://svn.code.sf.net/p/jaer/code/scripts/matlab
+% [allAddr,allTs]=loadaerdat(filename);
+% [TD.x,TD.y,TD.p]=extractRetinaEventsFromAddr(addr);
+% TD.x = TD.x+1; % difference in convention between DVS and ATIS
+% TD.y = TD.y+1; % difference in convention between DVS and ATIS
+% TD.p(TD.p==-1) = 0;
+% 
+% image = MakeImage(TD, [128,128], 0);
 
 %% save to a file
 image_number = 0; %which of the images is this (multiple images are required for calibration)
-imwrite(image,['CalibrationImages\img', num2str(image_number), '.bmp'], 'bmp')
+calibration_images_directory = 'CalibrationImages'; %name a directory to store the calibration images in
+if ~exist(calibration_images_directory, 'dir')
+    mkdir(calibration_images_directory) %if the directory doesn't already exist, create it
+end
+imwrite(calibration_image,[calibration_images_directory, '\img', num2str(image_number), '.bmp'], 'bmp')
 
-% now run the Caltech Camera Calibration toolbox and use the generated
+%% now run the Caltech Camera Calibration toolbox and use the generated
 % images for calibration. The toolbox is available from:
 % http://www.vision.caltech.edu/bouguetj/calib_doc/
+
+caltech_calibration_toolbox_path = 'TOOLBOX_calib'; % the path to the toolbox
+addpath(genpath(caltech_calibration_toolbox_path)) %add the toolbox to your path
+cd(calibration_images_directory) %change to the directory containing the calibration images
+calib_gui %run the calibration toolbox
